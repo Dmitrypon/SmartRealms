36a0ac7ca32e5bd17a5a4bf2c56a0a491b796eff
Dmitry Ponomar <mitrybox@gmail.com>
2023-10-14 21:57:14 +0300

    SmartRealms.MQTT.appsettings.json deleted

--- SmartRealms.API/Controllers/DataController.cs
+++ SmartRealms.API/Controllers/DataController.cs
@@ -10,13 +10,13 @@ namespace SmartRealms.API.Controllers
         public class DataController : ControllerBase
         {
             private readonly CommandDataRepository _repository;
-             
+
             public DataController(CommandDataRepository repository)
             {
                 _repository = repository;
             }
 
-            //[HttpGet("{id}")]
+            [HttpGet("{id}")]
             //public async Task<ActionResult<DataDto>> Get(int id, DataDto dataDto)
             //{
             //    var data = await _repository.GetLastDataAsync(id, HttpContext.RequestAborted);
@@ -24,24 +24,6 @@ namespace SmartRealms.API.Controllers
             //    return Ok(dataDto(data));
             //}
 
-
-
-            public static class DataDto
-            {
-                private static readonly object data;
-
-                public static string Name { get; set; }
-                public static double Value { get; set; }
-                public static DateTimeOffset Registered { get; set; }
-                public static int DeviceId { get; set; }
-
-                //public DataDto() { }
-
-                //public DataDto(object data)
-                //{
-                //    this.data = data;
-                //}            
-            }
         }
     }
 }
